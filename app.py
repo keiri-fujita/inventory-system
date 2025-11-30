a61c7591a8d9be828c5c1d089179ea3aac697b99
keiri-fujita <keiri@yoshiki-trading.comgit config --global user.name>
2025-11-30 22:06:01 +0900

    fix: add flash import

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 import csv
 import os
 from collections import defaultdict
